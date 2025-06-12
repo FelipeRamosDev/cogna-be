5cd0e5a8bdd0e3b78194f362bd9d4bdc479b28e2
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-06-12 17:28:17 -0300

    [COGB-12] Rota /product/:id (#11)
    ## [COGB-12] Descrição
    A rota /product/:id foi implementada para devolver o produto requisitado pelo parâmetro "id".

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,7 +2,7 @@ const DUMMY_PRODUCTS = require('../resources/dummy_products.json');
 
 module.exports = (req, res) => {
    if (!DUMMY_PRODUCTS) {
-      res.status(404).send({ error: true, message: 'Products not found' });
+      res.status(404).send({ error: true, message: 'Products resource not found!' });
       return;
    }
 
