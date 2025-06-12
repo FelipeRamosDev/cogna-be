8702d66d5d197819270913b3f7b6091e511951d6
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-06-12 17:15:33 -0300

    [COGB-11] Rota / (#10)
    ## [COGB-11] Descrição
    A rota "/" que devolve uma lista de produtos foi implementada.

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,13 @@
+const DUMMY_PRODUCTS = require('../resources/dummy_products.json');
+
+module.exports = (req, res) => {
+   if (!DUMMY_PRODUCTS) {
+      res.status(404).send({ error: true, message: 'Products not found' });
+      return;
+   }
+
+   res.status(200).send({
+      success: true,
+      products: DUMMY_PRODUCTS
+   });
+}
---
+++ src/routes/index.route.js
@@ -0,0 +1,6 @@
+const Route = require('../services/Route');
+
+module.exports = new Route({
+   path: '/',
+   method: 'GET'
+});
