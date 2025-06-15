5ee01855659b946f2581130dcfb7ae9cf321bce6
FelipeRamosDev <felipe@feliperamos.info>
2025-06-15 13:15:02 -0300

    [COGB-27] Removendo console.log esquecido

--- src/app.js
+++ src/app.js
@@ -2,21 +2,6 @@ const APIServer = require('./services/APIServer');
 const users_schema = require('./schemas/users_schema');
 const product_schema = require('./schemas/products_schema');
 
-console.log({
-   onListen: () => console.log('The APIServer was initialized!'),
-   port: process.env.SERVER_PORT,
-   database: {
-      type: process.env.DB_TYPE,
-      user: process.env.DB_USER,
-      dbName: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      password: process.env.DB_PASSWORD,
-      schemas: [
-         users_schema,
-         product_schema
-      ],
-   }
-})
 const apiServer = new APIServer({
    onListen: () => console.log('The APIServer was initialized!'),
    port: process.env.SERVER_PORT,
